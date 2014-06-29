22624ee4e9e40c220d6044975e0c9ab11fbcffcf
doctorwho1986 <doctorwho1986@foxmail.com>
2014-06-29 13:39:19 +0800

    项目初始化

---
+++ src/main/java/com/doctorwho/com/refection/App.java
@@ -0,0 +1,13 @@
+package com.doctorwho.com.refection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
